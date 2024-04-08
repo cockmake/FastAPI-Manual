b231539f2eddfb73fb8728d2ebf3f7f913e46068
cockmake <2359016829@qq.com>
2024-04-08 14:35:44 +0800

    修改异步Redis配置

--- curd/redis_pool_aio.py
+++ curd/redis_pool_aio.py
@@ -3,7 +3,7 @@ from redis import asyncio as aioredis
 from settings import REDIS_POOL_SIZE, REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_PASSWORD
 
 REDIS_CONFIG = {
-    "max_connections": REDIS_POOL_SIZE,  # 最大连接数比最大可工作线程数多1
+    # "max_connections": REDIS_POOL_SIZE,  # 最大连接数比最大可工作线程数多1
     "host": REDIS_HOST,
     "port": REDIS_PORT,
     "db": REDIS_DB,
